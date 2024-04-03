4c731424e7fcec0f4c3bd197c52b274da66ef6b0
Biruk Kebede <biruk.key@gmail.com>
2024-04-03 20:24:35 +0100

    Adding features section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -20,12 +20,24 @@ async function getDayActivities() {
 	return res.json()
 }
 
+async function getFeatures() {
+	const res = await fetch('http://localhost:8000/features')
+
+	if (!res.ok) {
+		throw new Error('Failed to fetch data')
+	}
+
+	return res.json()
+}
+
 export default async function HomePage() {
-	const intro: object | any = await getIntro()
-	const introObject = intro[0]
+	const intro = await getIntro()
+	// const introObject = intro[0]
 
 	const activities = await getDayActivities()
 	activities.sort((a: object | any, b: object | any) => a.id - b.id)
 
-	return <Home intro={introObject} day={activities} />
+	const features = await getFeatures()
+
+	return <Home intro={intro} day={activities} features={features} />
 }
