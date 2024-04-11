43181b940ef7b31135b0522a12b414a969589818
Biruk Kebede <biruk.key@gmail.com>
2024-04-12 00:02:11 +0100

    build files python version

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,2 @@
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --no-input --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic --no-input --clear
