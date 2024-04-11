9d26dbdf96247a5d2f6ac7b39dc9a3f276631f44
Biruk Kebede <biruk.key@gmail.com>
2024-04-11 03:02:21 +0100

    Minor changes

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,2 @@
-python3.12.2 -m pip install -r requirements.txt
-python3.12.2 manage.py collectstatic --no-input --clear
+pip install -r requirements.txt
+python manage.py collectstatic --no-input --clear
